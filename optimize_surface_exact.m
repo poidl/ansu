602d5258d9565f1d2fd609d6ee9e41743492be26
stefan <hoitaus@gmail.com>
2013-04-30 10:08:13 +1000

    Wrote function solve_lsqr() and dz_from_phiprime() that are called in the main iteration loop of optimize_surface_exact.m to make it more modular. It's now easy to understand the structure of optimize_surface_exact.

--- optimize_surface_exact.m
+++ optimize_surface_exact.m
@@ -1,203 +1,190 @@
 function [sns_i,ctns_i,pns_i] = optimize_surface_exact(s,ct,p,g,n2,sns,ctns,pns,e1t,e2t)
 
-%           Optimize density surfaces to minimise the fictitious diapycnal diffusivity
-%
-% Usage:    [sns_i,ctns_i,pns_i] =
-%           Optimize_surface(s,ct,p,g,n2,sns,ctns,pns,e1t,e2t,nit,choice,wrap)
-%
-%           Optimize density surface using an iterative method to minimise
-%           fictitious diapycnal diffusivity according 'A new method of
-%           forming approximately neutral surfaces, Klocker et al., Ocean
-%           Science, 5, 155-172, 2009.'
-%
-%
-% Input:    s           salinity
-%           ct          conservative temperature
-%           p           pressure
-%           g           gravitational acceleration
-%           n2          buoyancy frequency
-%           sns         salinity on initial density surface
-%           ctns        conservative temperature on initial density
-%                       surface
-%           pns         pressure on initial density surface
-%           e1t         grid length in meters in x-direction
-%           e2t         grid length in meters in y-direction
-%
-% %           settings    set in main window.
-% %           nit         maximum number of iterations is set to 150
-% %           choice      choice between
-% %                       's' slope error
-% %                       'epsilon' density gradient error
-% %           wrap        'none'
-% %                       'long'
-% %           solver      'iterative'
-% %                       'exact'
-% Output:   sns_i       salinity on optimized surface
-%           ctns_i      conservative temperature on optimized surface
-%           pns_i       pressure on optimized surface
-%
-% Calls:    mld.m, slope_error.m, var_on_surf.m
-%
-% Units:    salinity                    psu (IPSS-78)
-%           conservative temperature    degrees C (IPS-90)
-%           pressure                    dbar
-%           gravitational acceleration  m/s^2
-%           buoyancy frequency          s^-1
-%           scale factors               m
-%
-%   _________________________________________________________________
-%   This is part of the analyze_surface toolbox, (C) 2009 A. Klocker
-%   Partially modified by P. Barker (2010-13)
-%   Partially modified by S. Riha (2013)
-%   type 'help analyze_surface' for more information
-%   type 'analyze_surface_license' for license details
-%   type 'analyze_surface_version' for version details
-%
+    %           Optimize density surfaces to minimise the fictitious diapycnal diffusivity
+    %
+    % Usage:    [sns_i,ctns_i,pns_i] =
+    %           Optimize_surface(s,ct,p,g,n2,sns,ctns,pns,e1t,e2t,nit,choice,wrap)
+    %
+    %           Optimize density surface using an iterative method to minimise
+    %           fictitious diapycnal diffusivity according 'A new method of
+    %           forming approximately neutral surfaces, Klocker et al., Ocean
+    %           Science, 5, 155-172, 2009.'
+    %
+    %
+    % Input:    s           salinity
+    %           ct          conservative temperature
+    %           p           pressure
+    %           g           gravitational acceleration
+    %           n2          buoyancy frequency
+    %           sns         salinity on initial density surface
+    %           ctns        conservative temperature on initial density
+    %                       surface
+    %           pns         pressure on initial density surface
+    %           e1t         grid length in meters in x-direction
+    %           e2t         grid length in meters in y-direction
+    %
+    % %           settings    set in main window.
+    % %           nit         maximum number of iterations is set to 150
+    % %           choice      choice between
+    % %                       's' slope error
+    % %                       'epsilon' density gradient error
+    % %           wrap        'none'
+    % %                       'long'
+    % %           solver      'iterative'
+    % %                       'exact'
+    % Output:   sns_i       salinity on optimized surface
+    %           ctns_i      conservative temperature on optimized surface
+    %           pns_i       pressure on optimized surface
+    %
+    % Calls:    mld.m, slope_error.m, var_on_surf.m
+    %
+    % Units:    salinity                    psu (IPSS-78)
+    %           conservative temperature    degrees C (IPS-90)
+    %           pressure                    dbar
+    %           gravitational acceleration  m/s^2
+    %           buoyancy frequency          s^-1
+    %           scale factors               m
+    %
+    %   _________________________________________________________________
+    %   This is part of the analyze_surface toolbox, (C) 2009 A. Klocker
+    %   Partially modified by P. Barker (2010-13)
+    %   Partially modified by S. Riha (2013)
+    %   type 'help analyze_surface' for more information
+    %   type 'analyze_surface_license' for license details
+    %   type 'analyze_surface_version' for version details
+    %
 
-settings;
-[zi,yi,xi] = size(s);
+    settings;
+    [zi,yi,xi] = size(s);
 
-%% calculate buoyancy frequency on density surface
+    % mid-point pressure % TODO: this is only needed to interpolate n2 onto
+    % the surface; should be done in var_on_surf() ?
+    p_mid = 0.5*(p(2:zi,:,:) + p(1:zi-1,:,:));
 
-p_mid = 0.5*(p(2:zi,:,:) + p(1:zi-1,:,:));
+    % prepare data
+    cut_off_choice(1,1:yi,1:xi) = mld(s,ct,p);
 
+    % iterations of inversion
+    it=0; % start with it=0 and increment it after the initial surface is written to output
+    while it<=nit;
 
-%% prepare data
+        % calculate slope errors/density gradient errors
+        [ss,sx,sy,curl_s,ee,ex,ey,curl_e,ver] = slope_error(p,g,n2,sns,ctns,pns,e1t,e2t,'bp',wrap); %#ok
 
-
-cut_off_choice(1,1:yi,1:xi) = mld(s,ct,p);
-
-
-%% iterations of inversion
-    
-it=0; % start with it=0 and increment it after the initial surface is written to output
-while it<=nit;
-    
-    n2_ns = var_on_surf(pns,p_mid,n2);
-
-    %% calculate slope errors/density gradient errors on initial density surface
-
-    [ss,sx,sy,curl_s,ee,ex,ey,curl_e,ver] = slope_error(p,g,n2,sns,ctns,pns,e1t,e2t,'bp',wrap); %#ok
-    
-    
-    %% diagnose
-    if save_iterations;
-        if it==0; % dummy value for phiprime_e 
-            phiprime_e=nan; 
+        % diagnose
+        if save_iterations;
+            if it==0; % dummy value for phiprime
+                phiprime=nan;
+            end
+            diagnose_and_write(it,sns,ctns,pns,ex,ey,phiprime);
         end
-        diagnose_and_write(it,sns,ctns,pns,ex,ey,phiprime_e);
+
+        if it==nit; % break out if done
+            break
+        end
+
+        it=it+1; % start the next iteration
+        disp(['Iteration nr.',int2str(it)]);
+
+        % choice between minimizing slope errors or density gradient errors
+        switch choice
+            case 's'
+                xx = sx;
+                yy = sy;
+            case 'epsilon'
+                xx = ex;
+                yy = ey;
+        end
+
+        % calculate buoyancy frequency on density surface
+        n2_ns = var_on_surf(pns,p_mid,n2);
+
+        % disregard data above mixed layer depth
+        if it>0;
+            xx(pns<= cut_off_choice)=nan;
+            yy(pns<= cut_off_choice)=nan;
+        end
+        n2_ns(pns<=cut_off_choice)=nan;
+        pns(pns<=cut_off_choice)=nan;
+
+        xx = squeeze(xx);
+        yy = squeeze(yy);
+        pns = squeeze(pns);
+        n2_ns = squeeze(n2_ns);
+
+        % find independent regions -> a least-squares problem is solved for
+        % each of these regions
+        regions=find_regions(n2_ns);
+
+        % solve for phiprime
+        phiprime=solve_lsqr(regions, xx, yy, e1t, e2t);
+
+        % find corrected surface
+        [sns, ctns, pns] = dz_from_phiprime(sns, ctns, pns, s, ct, p, phiprime );
+
     end
-    
-    
-    if it==nit;
-        break
-    end
-    
-    it=it+1;
-    disp(['Iteration nr.',int2str(it)]); % print number of iteration
 
-    %% choice between minimizing slope errors or density gradient errors
+    sns_i=sns;
+    ctns_i=ctns;
+    pns_i=pns;
 
-    switch choice
-        case 's'
-            xx = sx;
-            yy = sy;
-        case 'epsilon'
-            xx = ex;
-            yy = ey;
-    end
-    
-    if it>0;
-        xx(pns<= cut_off_choice)=nan;
-        yy(pns<= cut_off_choice)=nan;
-    end
-    
-    xx_squeeze = squeeze(xx);
-    yy_squeeze = squeeze(yy);
-
-    %% disregard data above mixed layer depth
-    
-    n2_ns(pns<=cut_off_choice)=nan;
-    pns(pns<=cut_off_choice)=nan;
-    
-    pns = squeeze(pns);
-    n2_ns = squeeze(n2_ns);
+end
 
 
+function phiprime=solve_lsqr(regions, xx, yy, e1t, e2t)
+    settings;
+    [yi,xi]=size(xx);
     phiprime = nan(yi,xi);
-    phiprime_e = nan(yi,xi);
-    
-    % flag wet points
-    
-    wet=~isnan(n2_ns);
-    
-    % find independent regions -> a least-squares problem is solved for
-    % each of these regions
-    
-    regions=find_regions(wet);
-    
-    for nregion=1:length(regions)
-       
-        region=regions{nregion};
-        
 
-        %% set up east-west equations for weighted inversion
+    for iregion=1:length(regions) 
         
+        region=regions{iregion};
+
+        % set up east-west equations for weighted inversion
         reg=false(1,xi*yi)'; 
         reg(region)=true;
         
-        % centered finite differences on a staggered grid
-        en= reg & circshift(reg,-yi);
+        en= reg & circshift(reg,-yi); % find points where finite differences can be computed
         if strcmp(wrap,'none')  % remove equations for eastern boundary for zonally-nonperiodic domain
-            bdy_east=false(1,xi*yi); bdy_east(yi*(xi-1)+1:end)=true;
-            en=en & ~bdy_east(:);
+            not_bdy_east=true(1,xi*yi); not_bdy_east(yi*(xi-1)+1:end)=false;
+            en=en & not_bdy_east(:);
         end
-               
-        % j1 are j-indices for matrix coefficient 1
-        sreg=cumsum(reg); % sparse indices of region (points of non-region are indexed with dummy)
+        sreg=cumsum(reg); % sparse indices of points forming the region (points of non-region are indexed with dummy)
         sreg_en=circshift(sreg,-yi); % sparse indices of eastward neighbours
-        j1_ew=sreg_en(en); 
-       
+        
+        j1_ew=sreg_en(en);  % j1 are j-indices for matrix coefficient 1
         j2_ew=sreg(en); % j2 are j-indices for matrix coefficient -1
         
-
-        %% set up north-south equations for weighted inversion
-        
+        % set up north-south equations for weighted inversion
         nn= reg & circshift(reg,-1);
-        % remove equations for northern boundary
-        bdy_north=false(1,xi*yi); bdy_north(yi:yi:yi*xi)=true;
-        nn=nn & ~bdy_north(:);
-
+        not_bdy_north=true(1,xi*yi); not_bdy_north(yi:yi:yi*xi)=false; % remove equations for northern boundary
+        nn=nn & not_bdy_north(:);
         sreg_nn=circshift(sreg,-1);
+        
         j1_ns=sreg_nn(nn);
-        
         j2_ns=sreg(nn);
-        
-        j2=[j2_ew',j2_ns'];
-        
-        
-        %% make the average of Phi' zero 
+           
+        % make the average of Phi' zero 
         % this should keep the surface from drifting away from the initial condition
         % we might change that to a different condition
-        
         j1_condition=[1:sum(reg)];
+  
         j1=[j1_ew',j1_ns',j1_condition];
-        
+        j2=[j2_ew',j2_ns'];
         
         i2=1:(sum(en)+sum(nn)); % i-indices for matrix coeff. -1
-        i1=[i2, (sum(en)+sum(nn)+1)*ones(1,sum(reg))];
+        i1=[i2, (sum(en)+sum(nn)+1)*ones(1,sum(reg))]; % i-indices for matrix coeff. 1
         
         % build sparse matrices
         A=sparse([i1,i2],[j1,j2],[ones(1,length(i1)),-ones(1,length(i2))]);
-        b=sparse( [xx_squeeze(en).*e1t(en); yy_squeeze(nn).*e2t(nn); 0 ]);
+        b=sparse( [xx(en).*e1t(en); yy(nn).*e2t(nn); 0 ]);
         
-        disp(['solving for region ',int2str(nregion)]);
+        disp(['solving for region ',int2str(iregion)]);
         %stef: 'exact' gets to the solution quicker but requires more
         %memory
         switch solver
             case 'iterative'
                 [x,dummyflag] = lsqr(A,b,1e-7,50000);
-                
             case 'exact'
                 x = (A'*A)\(A'*b);
         end
@@ -206,19 +193,19 @@ while it<=nit;
         
         % put density changes calculated by the least-squares solver into
         % their appropriate position in the matrix
-        
         switch choice
             case 's'
                 phiprime(region) = - x;
             case 'epsilon'
-                phiprime_e(region) = x;
-        end
-        
+                phiprime(region) = x;
+        end   
     end
+end
 
-      
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [sns_out,ctns_out,pns_out] = dz_from_phiprime(sns, ctns, pns, s, ct, p, phiprime_e );
+
+    [zi,yi,xi]=size(s);
     dummy_phiprime_e(1,:,:) = (-1).*phiprime_e;
     
     r=1.0;
@@ -232,9 +219,9 @@ while it<=nit;
     s_tmp=s;
     ct_tmp=ct;
     p_tmp=p;
-    pns_tmp = nan(1,yi,xi);
-    sns_tmp = nan(1,yi,xi);
-    ctns_tmp = nan(1,yi,xi);
+    pns_out = nan(1,yi,xi);
+    sns_out = nan(1,yi,xi);
+    ctns_out = nan(1,yi,xi);
     pns=pns(:);
 
     refine_ints=100;
@@ -242,7 +229,6 @@ while it<=nit;
     cnt=0;
     while 1
         cnt=cnt+1;
-        inds=inds(fr);
          
         if cnt==1 % in first iteration pns_l is stacked vertically zi times, after that it is stacked refine_ints times
             stack=zi;
@@ -251,13 +237,14 @@ while it<=nit;
         end
         if cnt==1 | cnt==2
             ii=bsxfun(@times,1:yi*xi,ones(stack,1)); 
-            pns_3d=pns(ii);
-            t2_3d_full=t2(ii);
+            pns_stacked=pns(ii); % stack pressure of current surface vertically
+            t2_stacked_full=t2(ii); % stack locally referenced density of current surface vertically
         end
         
-        t1=gsw_rho(s_tmp(:,:),ct_tmp(:,:),pns_3d(:,inds));
-        t2_3d=t2_3d_full(:,inds);
-        tni=t1-t2_3d; % rho-(rho_s+rho')
+        inds=inds(fr); % points where surface has not been corrected
+        t1=gsw_rho(s_tmp(:,:),ct_tmp(:,:),pns_stacked(:,inds)); % 3-d density referenced to pressure of the current surface
+        t2_3d=t2_stacked_full(:,inds); %
+        tni=t1-t2_3d; % rho-(rho_s+rho'); find corrected surface by finding roots of this term
         
         Itni_p = tni>0;
         Itni_n = tni<0;
@@ -265,26 +252,26 @@ while it<=nit;
         zc = any(Itni_p,1) & any(Itni_n,1); % horizontal indices of locations where zero-crossing occurs
         [min_tni, lminr]=min(abs(tni));
         cond1=min_tni>delta; 
-        final=min_tni<=delta; 
-        fr= zc & cond1; % adjust surface depth here (find root)
+        final=min_tni<=delta; % at these horizontal indices root has been found
+        fr= zc & cond1; %  at these horizontal locations we have to increase the vertical resolution before finding the root
         
         lminr=lminr+stack*[0:size(Itni_n,2)-1];
         lminr=lminr(final);
         
-        sns_tmp(inds(final))=s_tmp(lminr);
-        ctns_tmp(inds(final)) =ct_tmp(lminr);
-        pns_tmp(inds(final)) =p_tmp(lminr);
+        sns_out(inds(final))=s_tmp(lminr); % adjust surface where root has already been found
+        ctns_out(inds(final)) =ct_tmp(lminr);
+        pns_out(inds(final)) =p_tmp(lminr);
 
 
-        if all(~fr)
+        if all(~fr) % break out of loop if all roots have been found
             break      
         end
 
-        k=sum(Itni_n,1);
+        k=sum(Itni_n,1); % find indices of flattened 3d-array where vertical resolution must be increased
         k=k+stack*[0:size(Itni_n,2)-1];
         k=k(fr); 
         
-        ds_ =  ( s_tmp(k+1) - s_tmp(k))/refine_ints;
+        ds_ =  ( s_tmp(k+1) - s_tmp(k))/refine_ints; % increase resolution in the vertical
         dct_ = (ct_tmp(k+1) - ct_tmp(k))/refine_ints;
         dp_ =  (p_tmp(k+1) - p_tmp(k))/refine_ints;
 
@@ -297,25 +284,16 @@ while it<=nit;
         p_tmp =  bsxfun(@plus,p_tmp(k),dp_);
 
     end
-      
-    pns=pns_tmp;
-    ctns=ctns_tmp;
-    sns=sns_tmp;
-   
-                        
-end
-
-    sns_i=sns;
-    ctns_i=ctns;
-    pns_i=pns;
-
 end
 
 
-function regions=find_regions(wet)
+function regions=find_regions(vv)
+
     settings;
+    [yi,xi]=size(vv);
     
-    [yi,xi]=size(wet);
+    % flag wet points (mixed layer excluded)
+    wet=~isnan(vv);
     
     cc=bwconncomp(wet,4);
 
