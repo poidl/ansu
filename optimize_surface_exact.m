5d3e6c274a43f75afc9aae7dc8d1e5b3801f0f84
stefan <hoitaus@gmail.com>
2013-06-25 14:35:13 +1000

    add original version of depth_ntp.m (by D. Jackett and G. Serazin) rename directory djackett to jackett_serazin

--- optimize_surface_exact.m
+++ optimize_surface_exact.m
@@ -192,117 +192,126 @@ sns_out = nan(1,yi,xi);
 ctns_out = nan(1,yi,xi);
 pns=pns(:);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% alternative code
 
-stack=zi;
-ii=bsxfun(@times,1:yi*xi,ones(stack,1));
-pns_stacked=pns(ii); % stack pressure of current surface vertically
-t2_stacked_full=t2(ii); % stack locally referenced density of current surface vertically
-
-t1=gsw_rho(s(:,:),ct(:,:),pns_stacked(:,inds)); % 3-d density referenced to pressure of the current surface
-t2_3d=t2_stacked_full(:,inds); %
-tni=t1-t2_3d; % rho-(rho_s+rho'); find corrected surface by finding roots of this term
-
-Itni_p = tni>0;
-Itni_n = tni<0;
-
-zc = any(Itni_p,1) & any(Itni_n,1); % horizontal indices of locations where zero-crossing occurs
-[min_tni, lminr]=min(abs(tni));
-cond1=min_tni>delta;
-final=min_tni<=delta; % at these horizontal indices root has been found
-fr= zc & cond1; %  at these horizontal locations we have to use root finding
-
-lminr=lminr+stack*[0:size(Itni_n,2)-1];
-lminr=lminr(final);
-
-sns_out(inds(final))=s(lminr); % adjust surface where root has already been found
-ctns_out(inds(final)) =ct(lminr);
-pns_out(inds(final)) =p(lminr);
-
-k=sum(Itni_n,1); % find indices of flattened 3d-array where we use root finding
-k=k+stack*[0:size(Itni_n,2)-1];
-
-
-options=optimset('TolX',1e-3);
-for ii=inds(fr);
-    su=s(k(ii)); sl=s(k(ii)+1); % linear interpolation of s and ct
-    ctu=ct(k(ii)); ctl=ct(k(ii)+1);
-    pu=p(k(ii)); pl=p(k(ii)+1);
-    
-    sp=@(p) su+(sl-su)*(p-pu)/(pl-pu);
-    ctp=@(p) ctu+(ctl-ctu)*(p-pu)/(pl-pu);
-    fac=1./sqrt(gsw_rho(su,ctu,pu)^2+gsw_rho(sl,ctl,pl)^2); % scale to avoid having to set tolerance ?
-    drho_normalized=@(p)  fac.*(gsw_rho(sp(p), ctp(p), pns(ii)) -t2(ii));
-    
-    %dmyrho_normalized=@(p,su,sl,ctu,ctl,pu,pl)  1.*(gsw_rho( su+(sl-su)*(p-pu)/(pl-pu), ctu+(ctl-ctu)*(p-pu)/(pl-pu), pns(ii))-t2(ii));
-    %drho_normalized=@(p) dmyrho_normalized(p,su,sl,ctu,ctl,pu,pl);
-    proot=fzero(drho_normalized, [pu,pl],options);
-    
-    sns_out(ii)= su+(sl-su)*(proot-pu)/(pl-pu);
-    ctns_out(ii)=ctu+(ctl-ctu)*(proot-pu)/(pl-pu);
-    pns_out(ii)=proot;
-end
-    
+% stack=zi;
+% ii=bsxfun(@times,1:yi*xi,ones(stack,1));
+% pns_stacked=pns(ii); % stack pressure of current surface vertically
+% t2_stacked_full=t2(ii); % stack locally referenced density of current surface vertically
 % 
-% refine_ints=100;
+% t1=gsw_rho(s(:,:),ct(:,:),pns_stacked(:,inds)); % 3-d density referenced to pressure of the current surface
+% t2_3d=t2_stacked_full(:,inds); %
+% tni=t1-t2_3d; % rho-(rho_s+rho'); find corrected surface by finding roots of this term
 % 
-% cnt=0;
-% while 1
-%     cnt=cnt+1;
+% Itni_p = tni>0;
+% Itni_n = tni<0;
+% 
+% zc = any(Itni_p,1) & any(Itni_n,1); % horizontal indices of locations where zero-crossing occurs
+% [min_tni, lminr]=min(abs(tni));
+% cond1=min_tni>delta;
+% final=min_tni<=delta; % at these horizontal indices root has been found
+% fr= zc & cond1; %  at these horizontal locations we have to use root finding
+% 
+% lminr=lminr+stack*[0:size(Itni_n,2)-1];
+% lminr=lminr(final);
+% 
+% sns_out(inds(final))=s(lminr); % adjust surface where root has already been found
+% ctns_out(inds(final)) =ct(lminr);
+% pns_out(inds(final)) =p(lminr);
+% 
+% k=sum(Itni_n,1); % find indices of flattened 3d-array where we use root finding
+% k=k+stack*[0:size(Itni_n,2)-1];
+%
+% options=optimset('TolX',1e-3);
+% for ii=inds(fr);
+%     su=s(k(ii)); sl=s(k(ii)+1); % linear interpolation of s and ct
+%     ctu=ct(k(ii)); ctl=ct(k(ii)+1);
+%     pu=p(k(ii)); pl=p(k(ii)+1);
 %     
-%     if cnt==1 % in first iteration pns_l is stacked vertically zi times, after that it is stacked refine_ints times
-%         stack=zi;
-%     elseif cnt==2
-%         stack=refine_ints+1;
-%     end
-%     if cnt==1 | cnt==2
-%         ii=bsxfun(@times,1:yi*xi,ones(stack,1));
-%         pns_stacked=pns(ii); % stack pressure of current surface vertically
-%         t2_stacked_full=t2(ii); % stack locally referenced density of current surface vertically
-%     end
+%     sp=@(p) su+(sl-su)*(p-pu)/(pl-pu);
+%     ctp=@(p) ctu+(ctl-ctu)*(p-pu)/(pl-pu);
+%     fac=1./sqrt(gsw_rho(su,ctu,pu)^2+gsw_rho(sl,ctl,pl)^2); % scale to avoid having to set tolerance ?
+%     drho_normalized=@(p)  fac.*(gsw_rho(sp(p), ctp(p), pns(ii)) -t2(ii));
 %     
-%     inds=inds(fr); % points where surface has not been corrected
-%     t1=gsw_rho(s(:,:),ct(:,:),pns_stacked(:,inds)); % 3-d density referenced to pressure of the current surface
-%     t2_3d=t2_stacked_full(:,inds); %
-%     tni=t1-t2_3d; % rho-(rho_s+rho'); find corrected surface by finding roots of this term
+%     %dmyrho_normalized=@(p,su,sl,ctu,ctl,pu,pl)  1.*(gsw_rho( su+(sl-su)*(p-pu)/(pl-pu), ctu+(ctl-ctu)*(p-pu)/(pl-pu), pns(ii))-t2(ii));
+%     %drho_normalized=@(p) dmyrho_normalized(p,su,sl,ctu,ctl,pu,pl);
+%     proot=fzero(drho_normalized, [pu,pl],options);
 %     
-%     Itni_p = tni>0;
-%     Itni_n = tni<0;
-%     
-%     zc = any(Itni_p,1) & any(Itni_n,1); % horizontal indices of locations where zero-crossing occurs
-%     [min_tni, lminr]=min(abs(tni));
-%     cond1=min_tni>delta;
-%     final=min_tni<=delta; % at these horizontal indices root has been found
-%     fr= zc & cond1; %  at these horizontal locations we have to increase the vertical resolution before finding the root
-%     
-%     lminr=lminr+stack*[0:size(Itni_n,2)-1];
-%     lminr=lminr(final);
-%     
-%     sns_out(inds(final))=s(lminr); % adjust surface where root has already been found
-%     ctns_out(inds(final)) =ct(lminr);
-%     pns_out(inds(final)) =p(lminr);
-%     
-%     
-%     if all(~fr) % break out of loop if all roots have been found
-%         break
-%     end
-%     
-%     k=sum(Itni_n,1); % find indices of flattened 3d-array where vertical resolution must be increased
-%     k=k+stack*[0:size(Itni_n,2)-1];
-%     k=k(fr);
-%     
-%     ds_ =  ( s(k+1) - s(k))/refine_ints; % increase resolution in the vertical
-%     dct_ = (ct(k+1) - ct(k))/refine_ints;
-%     dp_ =  (p(k+1) - p(k))/refine_ints;
-%     
-%     ds_ =bsxfun(@times, ds_, [0:refine_ints]');
-%     dct_ = bsxfun(@times, dct_, [0:refine_ints]');
-%     dp_ = bsxfun(@times, dp_, [0:refine_ints]');
-%     
-%     s =  bsxfun(@plus,s(k),ds_);
-%     ct =  bsxfun(@plus,ct(k),dct_);
-%     p =  bsxfun(@plus,p(k),dp_);
-%     
+%     sns_out(ii)= su+(sl-su)*(proot-pu)/(pl-pu);
+%     ctns_out(ii)=ctu+(ctl-ctu)*(proot-pu)/(pl-pu);
+%     pns_out(ii)=proot;
 % end
+
+% alternative code
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% alternative code
+    
+refine_ints=100;
+
+cnt=0;
+while 1
+    cnt=cnt+1;
+    
+    if cnt==1 % in first iteration pns_l is stacked vertically zi times, after that it is stacked refine_ints times
+        stack=zi;
+    elseif cnt==2
+        stack=refine_ints+1;
+    end
+    if cnt==1 | cnt==2
+        ii=bsxfun(@times,1:yi*xi,ones(stack,1));
+        pns_stacked=pns(ii); % stack pressure of current surface vertically
+        t2_stacked_full=t2(ii); % stack locally referenced density of current surface vertically
+    end
+    
+    inds=inds(fr); % points where surface has not been corrected
+    t1=gsw_rho(s(:,:),ct(:,:),pns_stacked(:,inds)); % 3-d density referenced to pressure of the current surface
+    t2_3d=t2_stacked_full(:,inds); %
+    tni=t1-t2_3d; % rho-(rho_s+rho'); find corrected surface by finding roots of this term
+    
+    Itni_p = tni>0;
+    Itni_n = tni<0;
+    
+    zc = any(Itni_p,1) & any(Itni_n,1); % horizontal indices of locations where zero-crossing occurs
+    [min_tni, lminr]=min(abs(tni));
+    cond1=min_tni>delta;
+    final=min_tni<=delta; % at these horizontal indices root has been found
+    fr= zc & cond1; %  at these horizontal locations we have to increase the vertical resolution before finding the root
+    
+    lminr=lminr+stack*[0:size(Itni_n,2)-1];
+    lminr=lminr(final);
+    
+    sns_out(inds(final))=s(lminr); % adjust surface where root has already been found
+    ctns_out(inds(final)) =ct(lminr);
+    pns_out(inds(final)) =p(lminr);
+    
+    
+    if all(~fr) % break out of loop if all roots have been found
+        break
+    end
+    
+    k=sum(Itni_n,1); % find indices of flattened 3d-array where vertical resolution must be increased
+    k=k+stack*[0:size(Itni_n,2)-1];
+    k=k(fr);
+    
+    ds_ =  ( s(k+1) - s(k))/refine_ints; % increase resolution in the vertical
+    dct_ = (ct(k+1) - ct(k))/refine_ints;
+    dp_ =  (p(k+1) - p(k))/refine_ints;
+    
+    ds_ =bsxfun(@times, ds_, [0:refine_ints]');
+    dct_ = bsxfun(@times, dct_, [0:refine_ints]');
+    dp_ = bsxfun(@times, dp_, [0:refine_ints]');
+    
+    s =  bsxfun(@plus,s(k),ds_);
+    ct =  bsxfun(@plus,ct(k),dct_);
+    p =  bsxfun(@plus,p(k),dp_);
+    
+end
+
+% alternative code
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 end
 
